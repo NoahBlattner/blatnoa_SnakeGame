6a27f37e5198caaa45d1694b4e43477ff1769587
blatnoa <noahblattner04@gmail.com>
2023-01-16 16:36:02 +0100

    Modifié la classe interne de Snake pour les cellule du serpent

--- app/src/main/java/com/divtec/blatnoa/snakegame/Snake/Snake.java
+++ app/src/main/java/com/divtec/blatnoa/snakegame/Snake/Snake.java
@@ -1,6 +1,5 @@
 package com.divtec.blatnoa.snakegame.Snake;
 
-import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 
@@ -12,24 +11,29 @@ import java.util.ArrayList;
 
 public class Snake implements Tickable {
 
-    private static class SnakeCell {
-        private enum CellType {
-            HEAD,
-            BODY,
-            TAIL,
-            FOOD,
-            EMPTY
-        }
-        private final int x;
-        private final int y;
-        private final Direction direction;
-        private final CellType type;
+    protected static class GameCell {
+        public int x;
+        public int y;
+        public Direction direction = Direction.NONE;
 
-        public SnakeCell(int x, int y, Direction direction, CellType cellType) {
+        public GameCell(int x, int y, Direction direction) {
             this.x = x;
             this.y = y;
             this.direction = direction;
-            this.type = cellType;
+        }
+
+        public GameCell(int x, int y, boolean isFood) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof GameCell) {
+                GameCell other = (GameCell) obj;
+                return other.x == x && other.y == y;
+            }
+            return false;
         }
     }
 
@@ -40,9 +44,10 @@ public class Snake implements Tickable {
         UP(180),
         DOWN(0),
         LEFT(90),
-        RIGHT(270);
+        RIGHT(270),
+        NONE(-1);
 
-        final int rotation;
+        public final int rotation;
 
         /**
          * Enum constructor
@@ -80,10 +85,14 @@ public class Snake implements Tickable {
         }
     }
 
-    private final GridLayout grid;
-    private final SnakeCell[][] cells;
+    private GridLayout grid;
 
-    private Direction direction = Direction.RIGHT;
+    private SnakeAdapter adapter;
+    private final ArrayList<GameCell> bodyCells = new ArrayList<>();
+    private final GameCell head;
+    private final ArrayList<GameCell> foodCells = new ArrayList<>();
+
+    private Direction nextDirection = Direction.RIGHT;
 
     int moveTimeMS = 1000;
     int timeSinceLastMove = 0;
@@ -93,31 +102,130 @@ public class Snake implements Tickable {
      * @param playingFieldGrid The grid to use as playing field
      */
     public Snake(GridLayout playingFieldGrid) {
-        // Create cells
         this.grid = playingFieldGrid;
-        cells = new SnakeCell[grid.getColumnCount()][grid.getRowCount()];
+
+        // Bind to adapter
+        adapter = new SnakeAdapter(grid);
 
         TickManager.getTickManager().addTickObject(this);
 
-        // Create snake head
+        // Get approx center of grid
         int x = grid.getColumnCount() / 2;
         int y = grid.getRowCount() / 2;
 
-        cells[x][y] = new SnakeCell(x, y, direction, SnakeCell.CellType.HEAD);
+        // Create snake head
+        head = new GameCell(x, y, nextDirection);
 
         ImageView centerImage = (ImageView) grid.getChildAt(x + y * grid.getColumnCount());
         centerImage.setImageResource(R.drawable.snake_head);
         centerImage.setAdjustViewBounds(true);
+
+        createFood();
+    }
+
+    private int randomInt(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
     }
 
     public void turn(Direction newDirection) {
-        if (!direction.isOpposite(newDirection)) {
-            direction = newDirection;
+        if (!nextDirection.isOpposite(newDirection)) {
+            nextDirection = newDirection;
         }
     }
 
+    private void createFood() {
+        // Create food
+        int randX;
+        int randY;
+        do {
+            randX = (int) (Math.random() * grid.getColumnCount());
+            randY = (int) (Math.random() * grid.getRowCount());
+        } while (!positionFree(randX, randY));
+
+        foodCells.add(new GameCell(randX, randY, null));
+    }
+
+    private boolean positionFree(int x, int y) {
+        if (x < 0 || x >= grid.getColumnCount() || y < 0 || y >= grid.getRowCount()) {
+            return false;
+        }
+
+        if (head.x == x && head.y == y) {
+            return false;
+        }
+
+        for (GameCell cell : bodyCells) {
+            if (cell.x == x && cell.y == y) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isFoodCell(int x, int y) {
+        for (GameCell cell : foodCells) {
+            if (cell.x == x && cell.y == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void move() {
-        // TODO
+        // Move head
+        int newX = head.x;
+        int newY = head.y;
+        switch (nextDirection) {
+            case UP:
+                newY--;
+                break;
+            case DOWN:
+                newY++;
+                break;
+            case LEFT:
+                newX--;
+                break;
+            case RIGHT:
+                newX++;
+                break;
+        }
+
+        // Check if new position is out of bounds
+        if (!positionFree(newX, newY)) {
+            // TODO game over
+            return;
+        }
+
+        if (bodyCells.size() > 0) { // If has body add body cell to take the head's place
+        }
+        // Move head
+        head.x = newX;
+        head.y = newY;
+        head.direction = nextDirection;
+
+        boolean removeTail = true;
+        if (isFoodCell(newX, newY)) { // If new position is food
+            // Don't remove tail
+            removeTail = false;
+            foodCells.remove(0);
+
+            // Create new food
+            createFood();
+        }
+
+        GameCell firstBodyCell = null;
+        if (bodyCells.size() > 0) { // If has body
+            // Move body
+            firstBodyCell = new GameCell(head.x, head.y, nextDirection);
+            bodyCells.add(firstBodyCell);
+            if (removeTail) {
+                // Remove tail
+                bodyCells.remove(0);
+            }
+        }
+
+        // Update grid
+        adapter.updateGrid(head, firstBodyCell, foodCells);
     }
 
     @Override
